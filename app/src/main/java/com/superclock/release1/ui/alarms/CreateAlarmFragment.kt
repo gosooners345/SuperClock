7cfbe986d6f576ba1635cbb3a66f6e373f127204
Brandon Guerin <BoomerSooner12345@gmail.com>
2021-06-18 09:51:06 -0400

    Something new

--- app/src/main/java/com/superclock/release1/ui/alarms/CreateAlarmFragment.kt
+++ app/src/main/java/com/superclock/release1/ui/alarms/CreateAlarmFragment.kt
@@ -3,4 +3,5 @@ package com.superclock.release1.ui.alarms
 import androidx.fragment.app.Fragment
 
 class CreateAlarmFragment : Fragment() {
+
 }
---
+++ app/src/main/java/com/superclock/release1/utils/TimePickerUtil.kt
@@ -0,0 +1,26 @@
+@file:Suppress("DEPRECATION")
+
+package com.superclock.release1.utils
+
+import android.os.Build
+
+import android.widget.TimePicker
+
+
+object TimePickerUtil {
+    fun getTimePickerHour(tp: TimePicker): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            tp.hour
+        } else {
+            tp.currentHour
+        }
+    }
+
+    fun getTimePickerMinute(tp: TimePicker): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            tp.minute
+        } else {
+            tp.currentMinute
+        }
+    }
+}
