e86389c8350bbf097687aec02634179ed44fe481
Brandon Guerin <boomersooner12345@gmail.com>
2021-06-24 22:54:01 -0400

    Fixed notification bug. Fixed bugs in snooze and dismiss buttons. Got a working alarm now
    Signed-off-by: Brandon Guerin <BoomerSooner12345@gmail.com>

--- app/src/main/java/com/superclock/release1/App.kt
+++ app/src/main/java/com/superclock/release1/App.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
-import android.util.Log
 import android.widget.Toast
 
 
@@ -15,37 +14,25 @@ class App : Application() {
     }
 
     private fun createNotificationChannel(/*context: Context,importance Int,showBadge: Boolean,name:String,desc:String*/) {
+if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.O)
+{
+    Toast.makeText(this,"Testing Notifications",Toast.LENGTH_SHORT).show()
+    val serviceChannel = NotificationChannel(
+        CHANNEL_ID,
+        "Alarm Service Channel",
+        NotificationManager.IMPORTANCE_DEFAULT
+    )
 
-          /*val channelID = "${context.packageName}-$name"
-          val channel = NotificationChannel(channelID,name,importance)
-          channel.description=desc
-          channel.setShowBadge(showBadge)
-          val notificationManager = context.getSystemService(NotificationManager::class.java)
-          notificationManager.createNotificationChannel(channel)*/
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val serviceChannel = NotificationChannel(
-                    CHANNEL_ID,
-                    "Alarm Service Channel",
-                    NotificationManager.IMPORTANCE_DEFAULT
-                )
-                val manager = getSystemService(NotificationManager::class.java)
-                manager.createNotificationChannel(serviceChannel)
+    val manager = getSystemService(
+        NotificationManager::class.java
+    )
+    manager.createNotificationChannel(serviceChannel)
+}
+
             }
 
 
 
-
-           /* val serviceChannel = NotificationChannel(
-                CHANNEL_ID,
-                "Alarm Service Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            val manager = getSystemService(
-                NotificationManager::class.java
-            )
-            manager.createNotificationChannel(serviceChannel)*/
-
-    }
 
     companion object {
         const val CHANNEL_ID = "ALARM_SERVICE_CHANNEL"
--- app/src/main/java/com/superclock/release1/RingActivity.kt
+++ app/src/main/java/com/superclock/release1/RingActivity.kt
@@ -10,8 +10,6 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.superclock.release1.data.Alarm
 import com.superclock.release1.services.AlarmService
 import java.util.*
@@ -27,43 +25,43 @@ class RingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ring)
         dismiss = findViewById(R.id.activity_ring_dismiss)
         clock = findViewById(R.id.activity_ring_clock)
-        ButterKnife.bind(this)
-        dismiss!!.setOnClickListener {
-            fun onClick(v: View?) {
-                val intentService = Intent(applicationContext, AlarmService::class.java)
-                applicationContext.stopService(intentService)
-                finish()
-            }
-
-        }
         snooze = findViewById(R.id.activity_ring_snooze)
-        snooze!!.setOnClickListener {
-            fun onClick(v: View?) {
-                val calendar: Calendar = Calendar.getInstance()
-                calendar.timeInMillis = System.currentTimeMillis()
-                calendar.add(Calendar.MINUTE, 10)
-                val alarm = Alarm(
-                    Random().nextInt(Int.MAX_VALUE),
-                    calendar.get(Calendar.HOUR_OF_DAY),
-                    calendar.get(Calendar.MINUTE),
-                    "Snooze",
-                    true,
-                    created = System.currentTimeMillis(),
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
+        //ButterKnife.bind(this)
+
+        dismiss!!.setOnClickListener(View.OnClickListener {
+            val intentService = Intent(applicationContext, AlarmService::class.java)
+            applicationContext.stopService(intentService)
+            finish()
+        })
+
+        snooze!!.setOnClickListener(View.OnClickListener {
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = System.currentTimeMillis()
+            calendar.add(Calendar.MINUTE, 10)
+            val alarm = Alarm(
+                Random().nextInt(Int.MAX_VALUE),
+                calendar[Calendar.HOUR_OF_DAY],
+                calendar[Calendar.MINUTE],
+                "Snooze",
+                true,
+                created = System.currentTimeMillis(),
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
                 false
-                )
-                alarm.schedule(applicationContext)
-                val intentService = Intent(applicationContext, AlarmService::class.java)
-                applicationContext.stopService(intentService)
-                finish()
-            }
-        }
+            )
+            alarm.schedule(applicationContext)
+            val intentService = Intent(applicationContext, AlarmService::class.java)
+            applicationContext.stopService(intentService)
+            finish()
+        })
+
+
+
         animateClock()
     }
 
